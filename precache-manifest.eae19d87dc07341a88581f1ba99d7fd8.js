899e0fde89fc7e7e79571ee7d715599e5e142350
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2020-08-09 14:28:38 -0400

    Updates

---
+++ precache-manifest.eae19d87dc07341a88581f1ba99d7fd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57372de6b35861b57c5e4ea8a2198c3",
+    "url": "/agiledice/index.html"
+  },
+  {
+    "revision": "e9947351a50fbcb04258",
+    "url": "/agiledice/static/css/main.74d51593.chunk.css"
+  },
+  {
+    "revision": "c4e3f845bc69d7381560",
+    "url": "/agiledice/static/js/2.66e98397.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/agiledice/static/js/2.66e98397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9947351a50fbcb04258",
+    "url": "/agiledice/static/js/main.3dc31b45.chunk.js"
+  },
+  {
+    "revision": "953f2288b2044ed75fd2",
+    "url": "/agiledice/static/js/runtime-main.3fd09ce8.js"
+  }
+]);
